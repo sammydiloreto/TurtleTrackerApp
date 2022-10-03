0ad6a610fb152fd3e16d553d4144af57dcdc0dd2
sammydiloreto <samantha.diloreto@duke.edu>
2022-10-03 17:25:32 -0400

    Handling errors

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -59,6 +59,10 @@ for the_key, the_value in date_dict.items():
     if the_value == user_date:
         #Add matching keys to list
         matching_keys.append(the_key)
+        
+# Report whether no keys were found
+if len(matching_keys) == 0:
+    print(f"Sara was not located on {user_date}")
 
 #Reveal locations for each key in matching_keys
 for matching_key in matching_keys:
